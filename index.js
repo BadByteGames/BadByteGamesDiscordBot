0dc2a091ee125a3a28ac81d65c157dff475d171f
armedturret <rjwhitenh@outlook.com>
2019-02-14 20:06:55 -0500

    updated dad jokes function

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ function deleteCharacters(original, number){
     return str;
 }
 
+//returns array item if exists
+function returnReal(array, index){
+    if(array.length > index){
+        return array[index]+" ";
+    }
+    return "";
+}
+
 //add events
 client.on('ready', () => {
     client.user.setActivity("--help");
@@ -30,8 +38,6 @@ client.on('message', clientMessage => {
         clientMessage.channel.send("die");
     }
 
-    //%0.1 chance to reply with 'navy seals' copypasta
-
     //%10 chance to reply with a friendly message to NoiseGenerator
     if(clientMessage.author.id === "345633153781596160" && Math.floor(Math.random()*10) == 7){
         messages = FriendlyMessages.friendly;
@@ -41,8 +47,29 @@ client.on('message', clientMessage => {
 
     //%10 chance to say a dad joke
     const messageWords = msg.split(" ");
-    if(((messageWords[0] === 'I'  && messageWords[1] === 'am') || (messageWords[0] === 'I\'m') || (messageWords[0] === 'Im')) && Math.floor(Math.random()*10) == 3){
-        clientMessage.channel.send(`Hy ${messageWords[2]}, I\'m ${client.user.username}!`);
+    
+    if(Math.floor(Math.random()*10) === 3){
+        if(messageWords.length > 2 && messageWords[0] === 'I'  && messageWords[1] === 'am'){
+            if(messageWords.length > 5){
+                clientMessage.channel.send(`Hi ${messageWords[2]}, I\'m ${client.user.username}!`);
+            }else{
+                clientMessage.channel.send(`Hi ${returnReal(messageWords, 2) + returnReal(messageWords, 3) + returnReal(messageWords, 4)}, I\'m ${client.user.username}!`);
+            }
+        }else if(messageWords.length > 1 && ((messageWords[0] === 'I\'m') || (messageWords[0] === 'Im')) ){
+            if(messageWords.length > 4){
+                clientMessage.channel.send(`Hi ${messageWords[2]}, I\'m ${client.user.username}!`);
+            }else{
+                clientMessage.channel.send(`Hi ${returnReal(messageWords, 1) + returnReal(messageWords, 2) + returnReal(messageWords, 3)}, I\'m ${client.user.username}!`);
+            }
+        }
+    }
+    if(messageWords.length > 1 && ((messageWords.length > 2 && messageWords[0] === 'I'  && messageWords[1] === 'am') || (messageWords[0] === 'I\'m') || (messageWords[0] === 'Im')) && Math.floor(Math.random()*10) == 3){
+        if(messageWords.length <= 5){
+            clientMessage.channel.send(`Hi ${messageWords[2]}, I\'m ${client.user.username}!`);
+        }else{
+            clientMessage.channel.send(`Hi ${messageWords[2]}, I\'m ${client.user.username}!`);
+        }
+            
     }
 
     //return if msg is not a command
@@ -57,6 +84,7 @@ client.on('message', clientMessage => {
         clientMessage.channel.send("Command list sent to direct messages");
         var helpRichEmbed = new Discord.RichEmbed();
         helpRichEmbed.setTitle("Here is a list of commands:");
+        helpRichEmbed.setDescription("(I also assist in conversations with good stuff)");
         helpRichEmbed.addField("--help","sends a list of commands", true);
         helpRichEmbed.addField("--subscribe <stream>","subscribes to a notification stream", true);
         helpRichEmbed.addField("--unsubscribe <stream>","unsubscribes from a notification stream", true);
